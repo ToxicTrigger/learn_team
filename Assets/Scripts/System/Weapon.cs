593ff914f92e2b3f2968738f018fe893c1a4bfee
toxic-trigger <syoa1996@gmail.com>
2020-07-26 22:21:14 +0900

    [김시하](수정)Hp 구조 변경

--- Assets/Scripts/System/Weapon.cs
+++ Assets/Scripts/System/Weapon.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public ParticleSystem hitEffect;
+    public ParticleSystem attackEffect;
     public string target_tag, target_layer;
     public float cool_down = 0.2f;
     public float Damage = 1.0f;
@@ -22,27 +23,25 @@ public class Weapon : MonoBehaviour
         
     }
 
-    IEnumerator hit(float cool_down , AI ai )
+    IEnumerator hit(float cool_down , Damageable hp)
     {
-        if (ai.fsm)
+        if (hp)
         {
-            ai.fsm.updateState = false;
-            ai.damageable.Hp -= Damage;
+            if (!hp.guard || (hp.guard && Vector3.Dot(this.transform.forward.normalized, hp.transform.forward.normalized) < 0f) ) // 가드를 했는데 후면??
+            {
+                hp.Hp -= Damage;
+            }
         }
         
+        if( attackEffect ) this.attackEffect.Play();
         yield return new WaitForSeconds(cool_down);
-
-        if (ai.fsm)
-        {
-            ai.fsm.updateState = true;
-        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer(target_layer) && other.gameObject.tag.Equals(target_tag))
         {
-            StartCoroutine(hit(cool_down, other.GetComponent<AI>()));
+            StartCoroutine(hit(cool_down, other.GetComponent<Damageable>() ));
             var tmp= GameObject.Instantiate(hitEffect, other.transform.position, Quaternion.identity);
             Destroy( tmp.gameObject , 2.0f );
         }
