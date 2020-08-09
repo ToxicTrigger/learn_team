de5b20d058278b4bbc48c51fcfee5d5b1d4e46f9
toxic-trigger <syoa1996@gmail.com>
2020-08-09 20:39:12 +0900

    Auto stash before merge of "master" and "origin/master"

--- Assets/Resource/Script/Camera/FollowCam.cs
+++ Assets/Resource/Script/Camera/FollowCam.cs
@@ -14,14 +14,28 @@ using UnityEngine;
 public class FollowCam : MonoBehaviour
 {
     public Transform target;
-    // 맵의 경계를 어떻게 얻어올까? 
+    public float move_speed = 1.0f;
+    public bool stop = false ;
+    
+    // 맵의 경계를 어떻게 얻어올까?   
     void Start()
     {
         
     }
 
+    void move()
+    {
+        // 1. 맵의 경계라면  카메라가 움직이지 않도록 해야해! 
+        // 2. 대상을 추적하는 속도는 .. 알아서 정하도록 해주자.
+        // 3. 뭔가 어떤 상황에( this.stop ) 의해서 움직임이 차단되는 경우가 있을 수 있어. stop 상태를 체크하자. 
+        
+        if( this.stop ) return;
+        Vector3 tmp = transform.position;
+        this.transform.position = Vector3.Lerp(tmp, target.position, move_speed);
+    }
+
     void Update()
     {
-        
+        this.move();        
     }
 }
