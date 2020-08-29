8743029adcf3488277df77558d5ee604a88d5008
ToxicTrigger <syoa1996@gmail.com>
2020-08-30 00:03:51 +0900

    [김시하](추가)체력바 + 속성표시 UI 추가

---
+++ Assets/Resource/Script/UI/Hpbar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hpbar : MonoBehaviour
+{
+    public Damageable hp;
+    public Image image;
+    public SpriteRenderer renderer;
+
+    Material hp_shader;
+    void Start()
+    {
+        hp = hp ? hp : GetComponentInParent<Damageable>();
+        hp_shader = renderer.material;
+    }
+
+    void Update()
+    {
+        if( !hp ) return;
+        if( hp_shader ) 
+        {
+            hp_shader.SetFloat("_Power" , hp.Hp / hp.getMaxHp());
+        }
+    }
+}
